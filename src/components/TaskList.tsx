fc1ecb9f704ac85d62090a1c6fc6e4e91cf42dfa
douglasvinicio <douglas.vinicio@hotmail.com>
2022-10-23 16:02:25 -0400

    Solved

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -12,21 +12,52 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [completedTasks, setCompletedTasks] = useState<Task[]>([]);
+  const [toDoTasks, setToDoTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  useEffect(() => {
+    setCompletedTasks(tasks.filter(task => task.isComplete))
+    setToDoTasks(tasks.filter(task => !task.isComplete))
+  }, [tasks])
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return; //An empty string is by default false, the ! operator negates the current state. Being newTaskTitle = false and !newTaskTitle = true;  
+    const newTask =  {
+      id : Math.floor(Math.random() * 100),
+      title : newTaskTitle,
+      isComplete : false
+    }        
+
+    // A task cannot be saved if contains the same id then an already created task, find a way to check before saving it.     
+    // if (tasks.map(task => task.id === newTask.id ?
+    //   {...newTask,
+    //     id : Math.random()
+    //   } : task))
+    console.log(tasks)
+    setTasks(oldState => [...oldState, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const toogledTasks = tasks.map(task => task.id === id ? {
+      ...task, 
+      isComplete: !task.isComplete
+    } : task) 
+
+    setTasks(toogledTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
   }
 
   return (
+    <div className='wrapper'>    
     <section className="task-list container">
       <header>
         <h2>Minhas tasks</h2>
@@ -46,7 +77,7 @@ export function TaskList() {
 
       <main>
         <ul>
-          {tasks.map(task => (
+          {toDoTasks.map(task => (
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
@@ -60,15 +91,53 @@ export function TaskList() {
                 </label>
                 <p>{task.title}</p>
               </div>
-
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
                 <FiTrash size={16}/>
-              </button>
-            </li>
+              </button>              
+            </li>        
           ))}
-          
         </ul>
       </main>
     </section>
+    <section className="task-list container">
+           <header>
+              <h2>Completed</h2>
+
+      </header>
+      <main>
+        <ul>
+          {completedTasks.map(task => (
+            <li key={task.id}>
+              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+                <label className="checkbox-container">
+                  <input 
+                    type="checkbox"
+                    readOnly
+                    checked={task.isComplete}
+                    onClick={() => handleToggleTaskCompletion(task.id)}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+                <p>{task.title}</p>
+              </div>
+              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
+                <FiTrash size={16}/>
+              </button>              
+            </li>        
+          ))}
+        </ul>
+      </main>
+    </section>
+    </ div>
   )
 }
+
+
+
+// This is an alternative solution, a line of thought for future comparison
+  // const [completedTasks, setCompletedTasks] = useState<Task[]>([]);
+  // const [toDoTasks, setToDoTasks] = useState<Task[]>([]);
+
+  // useEffect(() => {
+  //   setCompletedTasks(tasks.map(task => task.isComplete ? task))
+  // },[])
